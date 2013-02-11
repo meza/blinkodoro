dbf29523cf209667b8c568747007f1dac192ff34
meza <meza@meza.hu>
2013-02-11 17:53:28 +0000

    Progress

--- src/main/java/Blinkodoro.java
+++ src/main/java/Blinkodoro.java
@@ -1,3 +1,12 @@
+import thingm.blink1.*;
+
 public class Blinkodoro {
 
+
+    public static void main(String[] args) {
+        Blink1 b = new Blink1();
+        b.open();
+        b.setRGB(200, 0, 0);
+
+    }
 }
